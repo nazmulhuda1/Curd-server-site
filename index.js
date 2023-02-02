aaabcf349430d7f50fa1f181ece45a07751a2b98
nazmulhuda1 <nazmulhuda3130@gmail.com>
2023-02-02 16:24:35 +0600

    mongodb-securty

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://curdOparetions:UzwffKj0zxSIcfch@cluster0.sednmxn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sednmxn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
